b5ce37c8ee92241fcc6b59f18c44c00bd1721bbb
Carlo Costa <carlo.costa92@gmail.com>
2025-02-24 15:29:21 +0100

    feat: insert km

---
+++ js/app.js
@@ -0,0 +1,13 @@
+window.alert("Benvenuto alla Boolean Trasporti,la preghiamo di fornirci i km da percorrere e la sua età anagrafica");
+
+//denizione variabile costo treno
+
+const price= parseInt(0.21);
+
+//richiesta chilometri da percorrere
+
+let km= parseInt(prompt("Inserisci quanti km vuoi percorrere"))
+
+// stampa dato inserito
+
+console.log(km)
